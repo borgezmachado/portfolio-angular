0cd1c8cc5026cd5d0746cb5238030881cbbf3ca2
Felipe <20241ctb0100030@estudantes.ifpr.edu.br>
2026-06-08 19:59:12 +0000

    alterações pedidas na atividade 11 e juntamente de projetos e contato

--- portfolio-angular/src/app/projetos/projetos.ts
+++ portfolio-angular/src/app/projetos/projetos.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-projetos',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './projetos.html',
-  styleUrl: './projetos.css',
+  styleUrl: './projetos.css'
 })
 export class Projetos {}
