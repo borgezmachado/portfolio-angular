1e9fce917cd374b1a0563b0ea02e33877d6b0397
Felipe <20241ctb0100030@estudantes.ifpr.edu.br>
2026-06-15 19:50:55 +0000

    Aula17: API de projetos e catalogo (MariaDB + JSON)

--- portfolio-angular/src/app/home/home.ts
+++ portfolio-angular/src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon'; // Adicione este import
 
 @Component({
   selector: 'app-home',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatIconModule], // Adicione aqui também
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
